e32c8d076e628d725b35d9a567018cdd9bd886c7
Vedant Singh <vedantpsj1@gmail.com>
2023-05-04 22:34:04 +0530

    added default lookup

--- src/routes/inventory-lookup-default.js
+++ src/routes/inventory-lookup-default.js
@@ -9,11 +9,7 @@ const inventory = require("../assets/inventory.json");
 module.exports.post = function (req, res) {
   // If the request does not contain the essential data to process,
   // finish early with a custom error message for the user to see.
-  if (
-    !req.body ||
-    !req.body.submission ||
-    !req.body.submission.show_title_for_inventory
-  ) {
+  if (!req.body || !req.body.submission || !req.body.submission.show_title) {
     // A user friendly error message can be shown to the user in One Blink Forms
     // by returning a 400 Status code and a JSON payload with a `message` property.
     // There is no character limit, however it is suggested to keep the message
@@ -24,7 +20,7 @@ module.exports.post = function (req, res) {
   }
 
   // Access the submission data from the request body.
-  const show_title = req.body.submission.show_title_for_inventory;
+  const show_title = req.body.submission.show_title;
   const items = inventory.filter(
     (inventoryItem) => inventoryItem.show_title === show_title
   );
@@ -44,11 +40,13 @@ module.exports.post = function (req, res) {
         (show_title + item.item)
           .replace(/ /g, "")
           .replace(/,/g, "")
-          .replace(/'/g, "") + "__inventory",
+          .replace(/'/g, "") + "__package",
       label: `${item.item} | ${item.category} Piece | Qty ${item.quantity}`,
       type: "checkboxes",
+      defaultValue: [],
       buttons: false,
       required: false,
+      readOnly: true,
       options: [
         {
           value: item.rent_for_2_weeks,
@@ -56,6 +54,8 @@ module.exports.post = function (req, res) {
         },
       ],
     });
+
+    checkBoxElement.defaultValue = [checkBoxElement.options[0].id];
 
     elements.push(checkBoxElement);
     const inputElement = CivicPlus.Forms.generateFormElement({
@@ -65,13 +65,13 @@ module.exports.post = function (req, res) {
           .replace(/,/g, "")
           .replace(/'/g, "") +
         "__input" +
-        "__inventory",
+        "__package",
       label: `Select quantity`,
       type: "number",
-      defaultValue: 1,
+      defaultValue: item.quantity,
       minNumber: 1,
       maxNumber: item.quantity,
-      readOnly: false,
+      readOnly: true,
       conditionallyShow: true,
       conditionallyShowPredicates: [
         {
@@ -89,7 +89,7 @@ module.exports.post = function (req, res) {
 
   console.log(calc);
   const calculation = CivicPlus.Forms.generateFormElement({
-    name: "calculation__inventory",
+    name: "calculation__package",
     label: `Calculation`,
     type: "calculation",
     required: true,
