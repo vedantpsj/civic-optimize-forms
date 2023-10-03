6b85b9f2a86952807da1d47fbab086c58851917d
Vedant Singh <vedantpsj1@gmail.com>
2023-10-03 17:17:01 +0530

    Fix: Concation of the HTML

--- src/yelp/hotel.js
+++ src/yelp/hotel.js
@@ -1,7 +1,5 @@
 var axios = require('axios');
 
-/* STYLES OF THE CARD */
-let html = `<style media="screen">.yelp-card-list-wrapper {max-width: 1200px;margin: 0 auto;}.yelp-card-list-wrapper {width: 100%;text-align: center;}/* end style */.yelp-single-card {width: 350px;/*height: 700px;*/}/* end style */.yelp-single-card {display: inline-block;margin: 2px auto;padding: 3px;background-color: #ffffff00;}/* end style */.yelp-event-content-wrapper {width: 300px !important;margin: 0 auto;padding: 5% 0;}/* end style */.yelp-event-image {outline: 3px solid #e4eded;outline-offset: -15px;width: 300px !important;height: 300px !important;object-fit: cover;/* object-position: right; */transition: .30s ease-in-out;}/* end style */.yelp-event-image:hover {outline-offset: -30px;}/* end style */.yelp-business-type {font-size: 14px;text-transform: uppercase;font-family: sans-serif;background-color: black;color: white;padding: 3%;}/* end style */.yelp-business-description {}/* end style */.yelp-title { height:60px; text-transform: uppercase;font-family: sans-serif;font-size: 1.2em !important;line-height: 1.5em;}/* end style */#yelp-business-url {padding: 10px 20px;display: block;text-align: center;text-decoration: none;background-color: black;color: white;text-transform: uppercase;font-size: 14px;transition: .30s ease-in-out;}/* end style */.yelp-business-details {height: 150px;font-size: 16px !important;text-align: left;}/* end style */#yelp-business-url:hover {background-color: #cfcfcf;color: black;}/* end style */.cardAccordion {background-color: #e2e1e1;color: #444;cursor: pointer;padding: 18px;width: 100%;border: none;text-align: left;outline: none;font-size: 15px;transition: 0.4s;text-align: center;}/* end style */.openUp, .cardAccordion:hover {background-color: #ccc;}/* end style */.cardPanel {padding: 0 18px;/* display: none; */height: 0px;background-color: white;overflow: hidden;transition: 0.5s;}</style><div class="yelp-card-list-wrapper">`;
 
 let accordionScript = `<script>console.log("Teako's packaged script works!"); var acc = document.getElementsByClassName("cardAccordion");var i;for (i = 0; i < acc.length; i++) {acc[i].addEventListener("click", function() {this.classList.toggle("openUp");var cardPanel = this.nextElementSibling;if (cardPanel.style.height === "300px") {cardPanel.style.height = "0px";} else {cardPanel.style.height = "300px";}});}</script>`;
 
@@ -13,6 +11,9 @@ exports.updateHotels = async function (hcmsToken, yelpToken) {
       'Authorization': `Bearer ${yelpToken}`
     }
   });
+  /* STYLES OF THE CARD */
+  let html = `<style media="screen">.yelp-card-list-wrapper {max-width: 1200px;margin: 0 auto;}.yelp-card-list-wrapper {width: 100%;text-align: center;}/* end style */.yelp-single-card {width: 350px;/*height: 700px;*/}/* end style */.yelp-single-card {display: inline-block;margin: 2px auto;padding: 3px;background-color: #ffffff00;}/* end style */.yelp-event-content-wrapper {width: 300px !important;margin: 0 auto;padding: 5% 0;}/* end style */.yelp-event-image {outline: 3px solid #e4eded;outline-offset: -15px;width: 300px !important;height: 300px !important;object-fit: cover;/* object-position: right; */transition: .30s ease-in-out;}/* end style */.yelp-event-image:hover {outline-offset: -30px;}/* end style */.yelp-business-type {font-size: 14px;text-transform: uppercase;font-family: sans-serif;background-color: black;color: white;padding: 3%;}/* end style */.yelp-business-description {}/* end style */.yelp-title { height:60px; text-transform: uppercase;font-family: sans-serif;font-size: 1.2em !important;line-height: 1.5em;}/* end style */#yelp-business-url {padding: 10px 20px;display: block;text-align: center;text-decoration: none;background-color: black;color: white;text-transform: uppercase;font-size: 14px;transition: .30s ease-in-out;}/* end style */.yelp-business-details {height: 150px;font-size: 16px !important;text-align: left;}/* end style */#yelp-business-url:hover {background-color: #cfcfcf;color: black;}/* end style */.cardAccordion {background-color: #e2e1e1;color: #444;cursor: pointer;padding: 18px;width: 100%;border: none;text-align: left;outline: none;font-size: 15px;transition: 0.4s;text-align: center;}/* end style */.openUp, .cardAccordion:hover {background-color: #ccc;}/* end style */.cardPanel {padding: 0 18px;/* display: none; */height: 0px;background-color: white;overflow: hidden;transition: 0.5s;}</style><div class="yelp-card-list-wrapper">`;
+  
   for (var i = 0; i < res.data.businesses.length; i++) {
     let cardHTML = `<!-- START CARD -->
       <div class="yelp-single-card"><div class="yelp-event-content-wrapper"><a href="${
@@ -48,10 +49,10 @@ exports.updateHotels = async function (hcmsToken, yelpToken) {
   html = html + `</div>` + accordionScript;
 
   console.log("HTML CARD BLOCKS****", html);
-  await updateHCMS(hcmsToken);
+  await updateHCMS(hcmsToken, html);
 };
 
-async function updateHCMS(hcmsToken) {
+async function updateHCMS(hcmsToken, html) {
   console.log("****************UPDATING HCMS CALLED!!! ");
   const res = await axios.put("https://content.civicplus.com/api/content/ut-sandycity/html/f03cd4b7-f35a-43c5-89e6-c4313c30c98a", {
     data: {
