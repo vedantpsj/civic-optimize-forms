502741a21ec9bbc44a5a56118475c26432a7fa89
Vedant Singh <vedantpsj1@gmail.com>
2024-03-20 19:36:44 +0530

    added changes

--- server/getCategories.js
+++ server/getCategories.js
@@ -1,6 +1,19 @@
 var axios = require("axios");
 
 async function getCategories(token, category, show_title) {
+  let categories = [];
+  try {
+    const allCategories = await getHCMSCategories(token);
+    categories = getCategoryFromName(category, show_title, allCategories);
+  } catch (error) {
+    console.log({ ...error });
+    categories = getCategoryFromName("", "", category);
+  }
+
+  return categories;
+}
+
+function getCategoryFromName(category, show_title, allCategories) {
   const categories = [
     {
       id: "c5369245-a81e-4e90-ab2d-1c7ba0a9ebea",
@@ -11,29 +24,73 @@ async function getCategories(token, category, show_title) {
       name: "Theater Rentals",
     },
   ];
-  try {
-    const allCategories = await getHCMSCategories(token);
-    const category1 = allCategories.items.find(
-      (e) => e.name.toLowerCase() === category.toLowerCase()
-    );
-    if (category1) {
-      categories.push({
-        id: category1.id,
-        name: category1.name,
-      });
+  const names = [category, show_title];
+
+  switch (show_title) {
+    case "Beauty and the Beast Jr": {
+      names.push("Beauty and the Beast");
+      names.push("Junior Show");
+      break;
     }
-    const category2 = allCategories.items.find(
-      (e) => e.name.toLowerCase() === show_title.toLowerCase()
-    );
-    if (category2) {
-      categories.push({
-        id: category2.id,
-        name: category2.name,
-      });
+    case "The Little Mermaid, Jr.": {
+      names.push("The Little Mermaid");
+      names.push("Junior Show");
+      break;
     }
-  } catch (error) {
-    console.log(error);
+    case "Beauty and the Beast, Jr. (2014)": {
+      names.push("Beauty and the Beast");
+      names.push("Junior Show");
+      break;
+    }
+    case "High School Musical, Jr.": {
+      names.push("High School Musical");
+      names.push("Junior Show");
+      break;
+    }
+    case "Into the Woods (2023)":
+    case "Into the Woods (2015)": {
+      names.push("Into the Woods");
+      break;
+    }
+    case "Seussical the Musical": {
+      names.push("Seussical");
+
+      break;
+    }
+    case "Seussical the Musical, Jr.": {
+      names.push("Seussical");
+      names.push("Junior Show");
+      break;
+    }
+    case "The Lion King, Jr.": {
+      names.push("The Lion King");
+      names.push("Junior Show");
+      break;
+    }
+    case "James and the Giant Peach, Jr.": {
+      names.push("James and the Giant Peach");
+      names.push("Junior Show");
+      break;
+    }
+
+    case "Beauty and the Beast (2008)": {
+      names.push("Beauty and the Beast");
+      break;
+    }
   }
+
+  names.forEach((name) => {
+    const cat = allCategories.items.find(
+      (e) => e.name.toLowerCase() === name.toLowerCase()
+    );
+    if (cat) {
+      categories.push({
+        id: cat.id,
+        name: cat.name,
+      });
+    }
+  });
+
   return categories;
 }
 
